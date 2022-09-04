af2b6d6302bd79fb3b7fbd2ba0035dd22079e865
Dev One <devone.softwaresolutions@gmail.com>
2022-09-03 21:54:03 -0300

    move car

--- js/car.mjs
+++ js/car.mjs
@@ -4,9 +4,25 @@ export class Car {
   constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
+    this.speed = 2;
     this.width = width;
     this.height = height;
     this.controls = new Controls();
+  }
+
+  update() {
+    if (this.controls.forward) {
+      this.y -= this.speed;
+    }
+    if (this.controls.reverse) {
+      this.y += this.speed;
+    }
+    if (this.controls.left) {
+      this.x -= this.speed;
+    }
+    if (this.controls.right) {
+      this.x += this.speed;
+    }
   }
 
   draw(ctx){
