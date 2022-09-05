cc2af699657c38598888509a92b971adcb63e7f3
Dev One <devone.softwaresolutions@gmail.com>
2022-09-04 22:22:49 -0300

    allow to transform car in any polygon

--- js/car.mjs
+++ js/car.mjs
@@ -19,6 +19,35 @@ export class Car {
     this.controls = new Controls();
   }
 
+  #createPolygon() {
+    const points = [];
+
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+
+    return points;
+  }
+
   #move() {
     if (this.controls.forward) {
       this.speed += this.acceleration;
@@ -68,25 +97,19 @@ export class Car {
 
   update(roadBorders) {
     this.#move();
+    this.polygon = this.#createPolygon();
     this.sensor.update(roadBorders);
   }
 
   draw(ctx){
-    ctx.save();
-
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
-
     ctx.beginPath();
-    ctx.rect(
-      -this.width / 2,
-      -this.height / 2,
-      this.width,
-      this.height
-    );
-    ctx.fill();
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
 
-    ctx.restore();
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
+
+    ctx.fill();
 
     this.sensor.draw(ctx);
   }
